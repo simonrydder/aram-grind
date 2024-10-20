c5ed667935ebda099d7faf8d23dbfd527567becc
simonrydder <66967123+simonrydder@users.noreply.github.com>
2024-10-20 20:57:14 +0200

    file clean-up

--- test/unit/lol/test_champion_data.py
+++
@@ -1,9 +0,0 @@
-from src.lol.champion_data import ChampionData
-
-
-def test_champion_data_has_id(aatrox: ChampionData):
-    assert aatrox.id == "Aatrox"
-
-
-def test_champion_data_has_name(aatrox: ChampionData):
-    assert aatrox.name == "Aatrox"
